a71debe769c8125ac9ad3a7d88fa81d680656134
Sankarshan Purkayastha <sankarshan@Sankarshans-MacBook-Pro.local>
2014-01-21 19:40:42 +0530

    Initial Commit

---
+++ 4th sem cse/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  4th sem cse
+//
+//  Created by Sankarshan Purkayastha on 21/01/14.
+//  Copyright (c) 2014 Sankarshan Purkayastha. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
